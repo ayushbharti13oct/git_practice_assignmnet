4e348be510add739f0728cec8c912f2792aa6568
Ayush Bharti <ayushbharti13oct@gmail.com>
2022-09-13 19:43:43 +0530

    Entered a Problem 1 and Problem 2

---
+++ login.js
@@ -0,0 +1,27 @@
+**Problem 1 :** Check whether a number is Prime or not
+
+
+for(let i=0;i<=13;i++){
+  let count=0
+  for(let j=1;j<=i;j++){
+    if(i%j==0){
+     count++ 
+  }
+  }
+  if(count==5){
+    console.log("prime")
+    }else{
+    console.log("Not Prime")
+    }
+  }
+
+Problem 2 : Check whether a string is palindrome or not.
+
+let strReverse = str.split("").reverse().join("");
+
+if (str === strReverse) {
+  console.log("Yes");
+} else {
+  console.log("No");
+}
+  
